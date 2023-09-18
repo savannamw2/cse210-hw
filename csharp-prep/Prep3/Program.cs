f1f325759c4bc017889066ccd9d2a55823226c38
savannamw2 <whi19068@byui.edu>
2023-09-18 17:27:43 -0600

    completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        while (true)
+        {
+
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+
+            
+
+            Console.WriteLine("Guess a positive number between 1-100! ");
+            
+            string guess = "";
+
+            while (true)
+            {   
+                Console.WriteLine("What is your guess? ");
+                string guessNumber = Console.ReadLine();
+                int chance = int.Parse(guessNumber);
+                if (number < chance)
+                {
+                    guess = "Too High";
+                    Console.WriteLine($"{guess}");
+                }
+                else if (number > chance) 
+                {
+                    guess = "Too Low";
+                    Console.WriteLine($"{guess}");
+
+                }
+                else 
+                {
+                Console.WriteLine("You guessed it! ");
+                break; 
+
+                }
+
+
+
+                
+            
+            }
+        }
     }
 }
