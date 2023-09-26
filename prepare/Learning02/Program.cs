b9b3138a4d0aa6770bcd3f352f2409f7adb1003d
savannamw2 <whi19068@byui.edu>
2023-09-26 10:10:01 -0600

    W03 Prepare: completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Pinterest";
+        job1._startYear = 2025;
+        job1._endYear = 2027;
+
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Spotify";
+        job2._startYear = 2027;
+        job2._endYear = 2030;
+
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Savanna Mays";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+    
     }
 }
